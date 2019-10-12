1ef7d77f673c1e8ea5f170b800bfbfad5aa67e0f
永田大祐 <dbank0208@gmail.com>
2019-10-12 15:23:12 +0900

    add

---
+++ argument.py
@@ -0,0 +1,10 @@
+class className():
+    def __init__(self, n):
+        self.a = n
+    
+    def myprint(self):
+        print(self.a)
+
+c=className("1234567")
+c.myprint()
+//https://www.headboost.jp/python-class/
